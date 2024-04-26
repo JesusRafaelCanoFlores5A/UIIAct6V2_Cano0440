629532d862333c9c2994f34eaf12e8c1d0d59d46
JESUS_RAFAEL_CANO_FLORES <a.21308051280440@cbtis128.edu.mx>
2024-04-26 23:36:48 +0000

    Updating1

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:canoSplash/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/Amor.jpg",
+      title: "Amor",
+      description: "Color Amor 089-07"),
+  ProductModel(
+      img: "assets/images/Beso.jpg",
+      title: "Beso",
+      description: "Color Beso 094-05"),
+  ProductModel(
+      img: "assets/images/Descanso.jpg",
+      title: "Descanso",
+      description: "Color Descanso 205-05"),
+];
